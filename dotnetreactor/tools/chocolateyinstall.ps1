d74d16a657bf6e69b9a5f448aea161e021cc86dd
ye4241 <ye4241@gmail.com>
2021-03-19 19:23:50 +0800

    add dotnetreactor;

---
+++ dotnetreactor/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$url = 'https://www.eziriz.com/download.php?download=dotnet_reactor_setup_6_5_0_0.exe'
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = '.NET Reactor'
+  checksum       = '091c8065ee1ea24de3eacd70d4ec6b7fcb3f419d834d4f1aca7cecc6e3ce36b0'
+  checksumType   = 'sha256'
+  silentArgs     = "/s"
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ dotnetreactor/update.ps1
@@ -0,0 +1,31 @@
+﻿import-module au
+
+$domain = 'https://www.eziriz.com'
+$releases = "$domain/reactor_history.htm"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+    # ".\dotnetreactor.nuspec"        = @{
+    #   "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
+    # }
+  }
+}
+
+function global:au_GetLatest {
+  $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  $re = '^download.php\?download=dotnet_reactor_setup_(\d_\d_\d_\d).exe'
+  $url = $downloadPage.Links | Where-Object href -match $re | Select-Object -First 1 -expand href
+  $version = ($url | Select-String -AllMatches $re).Matches.Groups[1].Value -replace "_", "."
+  @{
+    URL32 = "$domain/$url"
+    Version = $version
+    # ReleaseNotes = $releaseNotesUrl
+  }
+}
+
+update -ChecksumFor 32
