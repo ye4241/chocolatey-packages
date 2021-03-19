8a9a9f75fb2758f483bf3f5ee22fd8c84e08857a
ye4241 <ye4241@gmail.com>
2021-03-19 19:29:19 +0800

    add missing parameters;

--- dotnetreactor/tools/chocolateyinstall.ps1
+++ dotnetreactor/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.eziriz.com/download.php?download=dotnet_reactor_setup_6_5_0_0.exe'
   softwareName   = '.NET Reactor'
-  checksum       = ''
-  checksumType   = ''
+  checksum       = '091c8065ee1ea24de3eacd70d4ec6b7fcb3f419d834d4f1aca7cecc6e3ce36b0'
+  checksumType   = 'sha256'
   silentArgs     = "/s"
   validExitCodes = @(0)
 }
