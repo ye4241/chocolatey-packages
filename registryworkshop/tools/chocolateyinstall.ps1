9e3bbd82d3d14d332fcd8e26ce92f4136bb1c9bd
ye4241 <ye4241@gmail.com>
2021-03-21 03:22:53 +0800

    add registryworkshop package;

---
+++ registryworkshop/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = 'http://www.torchsoft.com/download/RegistryWorkshop.exe'
+  softwareName   = 'Registry Workshop*'
+  checksum       = 'cabbb998557c5e883200d082853ca0db1a9f0db66332a0731019b7485c2e95d0'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'          
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
+    
---
+++ registryworkshop/update.ps1
@@ -0,0 +1,28 @@
+﻿import-module au
+
+$domain = 'http://www.torchsoft.com'
+$releases = "$domain/en/download.html"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $response = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  $content = $response.Content
+  $items = ($content | Select-String '<td align="center" valign="middle">(.*?)</td>' -AllMatches).Matches
+  $url = ($items[0].Groups[1].Value | Select-String 'href="(.*?)"' -AllMatches).Matches[0].Groups[1].Value
+  $version = $items[3].Groups[1].Value
+  @{
+    URL32   = "$domain$url"
+    Version = $version
+  }
+}
+
+Update-Package -ChecksumFor 32
