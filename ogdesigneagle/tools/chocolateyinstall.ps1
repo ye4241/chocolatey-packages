684c1712af8652427804ed9a947284d78b4a4012
ye4241 <ye4241@gmail.com>
2021-03-23 23:27:28 +0800

    add ogdesigneagle package;

---
+++ ogdesigneagle/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = 'https://eaglefile.oss-cn-shenzhen.aliyuncs.com/releases/Eagle-2.0-build10.exe'
+  softwareName   = 'Eagle' 
+  checksum       = 'f567deedb2609e90d510cc0e30916bba6255592965b661ae9c0060ab0b338884'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0) 
+}
+Install-ChocolateyPackage @packageArgs 
---
+++ ogdesigneagle/update.ps1
@@ -0,0 +1,26 @@
+﻿import-module au
+
+$domain = 'https://eagle.cool'
+$releases = "$domain/check-for-update"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $response = Invoke-RestMethod -Method Get -Uri $releases
+  $url = 'https:' + $response.links.windows
+  $version = $response.version
+  @{
+    URL32   = $url
+    Version = $version
+  }
+}
+
+Update-Package -ChecksumFor 32
