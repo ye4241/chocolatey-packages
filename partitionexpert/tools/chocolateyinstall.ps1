5daa1cfbea9372dcbbff4d70ec98f5b5de8d9d9e
ye4241 <ye4241@gmail.com>
2021-03-23 11:02:58 +0800

    add partitionexpert package;

---
+++ partitionexpert/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = $env:ChocolateyPackageName
+
+$toolsDir = $Temp
+$zipFilePath = "$toolsDir\mde-free-setup.zip"
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileFullPath  = $zipFilePath
+  url           = 'https://disk-tool.com/download/mde/mde-free-setup.zip'
+  checksum      = 'a35c3eba15b8e28db6fb850596610c117de705989fd4f5e31b81aaf7d8dfa2f9'
+  checksumType  = 'sha256'
+  forceDownload = $true
+}
+Get-ChocolateyWebFile @packageArgs
+
+$packageArgs = @{
+  packageName  = $packageName
+  fileFullPath = $zipFilePath 
+  destination  = $toolsDir
+}
+Get-ChocolateyUnzip @packageArgs
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  file           = "$toolsDir\mde-free-setup.exe"
+  softwareName   = 'Macrorit Partition Expert Free*'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ partitionexpert/update.ps1
@@ -0,0 +1,28 @@
+﻿import-module au
+
+$domain = 'https://macrorit.com'
+$releases = "$domain/js/arrlang.json"
+$price = "$domain/js/mde-price.json"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $response = Invoke-RestMethod -Method Get -Uri $price
+  $url = $response.free.downloads[0].link
+  $response = Invoke-RestMethod -Method Get -Uri $releases
+  $version = ($response | Select-String '"mde":[\s\S]*?"ver": "(.*?)"' -AllMatches).Matches[0].Groups[1].Value
+  @{
+    URL32   = $url
+    Version = $version
+  }
+}
+
+Update-Package -ChecksumFor 32
