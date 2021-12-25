15b8f86cf6ab0d47ff38966ee5b07e9a12693d53
ye4241 <yepeng@islide.cc>
2021-12-25 09:58:55 +0800

    fix dotnetreactor slient args;

--- dotnetreactor/tools/chocolateyinstall.ps1
+++ dotnetreactor/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.eziriz.com/download.php?download=dotnet_reactor_setup_6_8_0_0.exe'
   softwareName   = '.NET Reactor'
-  checksum       = '5bb7aa6aa09e547ffb30bf32c8f5b4f853ae9ce236dcea5d1c8f6bd2c5a81417'
+  checksum       = '6a80bb553edf5aea6d54590e059586441ff42bd0e2bedc9323bc048753e18939'
   checksumType   = 'sha256'
-  silentArgs     = "/s"
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES"
   validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
