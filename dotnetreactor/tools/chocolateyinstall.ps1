e0fa509a758b4318b956adc01e4a4c38e92decb4
ye4241 <yepeng@islide.cc>
2022-04-28 19:17:58 +0800

    fix shasum for dotnetreactor;

--- dotnetreactor/tools/chocolateyinstall.ps1
+++ dotnetreactor/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.eziriz.com/download.php?download=dotnet_reactor_setup_6_8_0_0.exe'
   softwareName   = '.NET Reactor'
-  checksum       = '6a80bb553edf5aea6d54590e059586441ff42bd0e2bedc9323bc048753e18939'
+  checksum       = '73937d0043818fc63bf7048915899ccbbb2a459ef352b3fbd824a5035ec528a4'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES"
   validExitCodes = @(0)
