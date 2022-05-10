89f5bed045d883a3e822271ea7b94bbf9d3ed4ea
ye4241 <ye4241@gmail.com>
2022-05-10 16:22:20 +0800

    add safenet-authentication-client;

---
+++ safenet-authentication-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  url           = 'https://www.digicert.com/StaticFiles/SafeNetAuthenticationClient-x32.msi'
+  url64bit      = 'https://www.digicert.com/StaticFiles/SafeNetAuthenticationClient-x64.msi'
+  softwareName  = 'SafeNet Authentication Client*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
