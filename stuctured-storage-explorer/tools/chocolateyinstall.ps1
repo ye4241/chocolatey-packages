31c2bda0710d5b6d428ac6f8fc8f9d284ffbaca5
ye4241 <yepeng@islide.cc>
2022-07-20 12:11:51 +0800

    add stuctured-storage-explorer;

---
+++ stuctured-storage-explorer/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageName = $env:ChocolateyPackageName
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = 'https://github.com/ironfede/openmcdf/releases/download/v2.2.1.9/StructuredStorageXplorer.zip'
+  checksum       = '2f3f74c59e6aec63dfbe34cc2128d0feb87e712da599da16da49d5c085a6b185'
+  checksumType   = 'sha256'
+  specificFolder = 'StructuredStorageXplorer'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ stuctured-storage-explorer/update.ps1
@@ -0,0 +1,31 @@
+﻿import-module au
+
+$domain = 'https://github.com'
+$project = "ironfede/openmcdf"
+$file = "StructuredStorageXplorer.zip"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $releasePage = Invoke-WebRequest -UseBasicParsing -Uri "$domain/$project/releases/latest"
+  $content = $releasePage.Content
+  $match = ($content | Select-String "href=""/$project/releases/tag/(.*?)""" -AllMatches).Matches[0]
+  $tag = $match.Groups[1].Value
+  $version = $tag -replace "v", ""
+  $match = ($content | Select-String "href=""(/$project/releases/download/$tag/$file)""" -AllMatches).Matches[0]
+  $url = $domain + $match.Groups[1].Value
+  @{
+    Version = $version
+    Url32   = $url
+  }
+}
+
+Update-Package -ChecksumFor 32
