357ae6b30eafc92e69d1e366a9fc9e3c4c7c6617
ye4241 <yepeng@islide.cc>
2022-07-25 11:37:21 +0800

    add vcredist-aio package;

---
+++ vcredist-aio/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFileLocation = Join-Path $toolsDir "VisualCppRedist_AIO_x86_x64.zip"
+$exeFileLocation = Join-Path $toolsDir "VisualCppRedist_AIO_x86_x64.exe"
+
+$webFileArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileFullPath = $zipFileLocation
+  url          = 'https://github.com/abbodi1406/vcredist/releases/download/v0.61.0/VisualCppRedist_AIO_x86_x64_61.zip'
+  checksum     = ''
+  checksumType = 'sha256'
+}
+Get-ChocolateyWebFile @webFileArgs
+
+$unzipArgs = @{
+  fileFullPath = $zipFileLocation
+  destination  = $toolsDir
+}
+Get-ChocolateyUnzip @unzipArgs
+
+$processArgs = @{
+  exeToRun       = $exeFileLocation
+  statements     = "-y"
+  validExitCodes = @(0)
+}
+Start-ChocolateyProcessAsAdmin @processArgs
---
+++ vcredist-aio/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exeFileLocation = Join-Path $toolsDir "VisualCppRedist_AIO_x86_x64.exe"
+
+$packageArgs = @{
+  exeToRun = $exeFileLocation
+  silentArgs     = "/aiR"
+  validExitCodes = @(0)
+}
+Start-ChocolateyProcessAsAdmin @packageArgs
