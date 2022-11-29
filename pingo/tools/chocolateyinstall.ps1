280d6ef70012a95ee7d73024cfd9ab5ab561d64c
ye4241 <yepeng@islide.cc>
2022-11-29 10:28:29 +0800

    add pingo package;

---
+++ pingo/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageName = $env:ChocolateyPackageName
+$packageVersion = $env:ChocolateyPackageVersion
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url64bit       = 'https://css-ig.net/bin/pingo-win64.zip'
+  checksum64     = '844f674ecdaca4c428236ba3273b3a42c34a7f8a6eb5fcae4bfeb057c3f1e838'
+  checksumType64 = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ pingo/update.ps1
@@ -0,0 +1,31 @@
+﻿import-module au
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"       = "`$1'$($Latest.Url64)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $releasePage = Invoke-WebRequest -UseBasicParsing -Uri "https://css-ig.net/pingo"
+  $content = $releasePage.Content
+  $match = ($content | Select-String ">a(.*?) — <a id=""download"" href=""(.*?)"">" -AllMatches).Matches[0]
+  $version = '0.' + $match.Groups[1].Value
+  $url = $match.Groups[2].Value
+  $options = @{
+    Headers = @{
+      'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.56' 
+    }
+  }
+  @{
+    Version = $version
+    Url64   = $url
+    Options = $options
+  }
+}
+
+Update-Package -ChecksumFor 64
