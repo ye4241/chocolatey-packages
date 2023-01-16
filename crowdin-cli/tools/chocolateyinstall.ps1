e519b28e8fb81dc0ff3da0d41466c9328da7ecfe
Chocolatey <chocolatey@realdimensions.net>
2023-01-16 18:00:52 +0000

    AU: 1 updated - crowdin-cli [skip ci] https://gist.github.com/efab467283c72d4d480ebab4829cc1f8/b4da12508e00c2cb52ba0e0a83f08ade0f86b0c5

--- crowdin-cli/tools/chocolateyinstall.ps1
+++ crowdin-cli/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageVersion = $env:ChocolateyPackageVersion
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/crowdin/crowdin-cli/releases/download/3.9.1/crowdin-cli.zip'
-  checksum      = '9f97837b6f7e1e11f860734bcd1848a71d9c787d69c344da5dd4ed55960d4f8f'
+  url           = 'https://github.com/crowdin/crowdin-cli/releases/download/3.9.2/crowdin-cli.zip'
+  checksum      = '24f353745aa6a612e244472d6edb695d14155e5acd081844e190fda06f91ae90'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
