1e70c5e9df998db2740e1607d66b3d1626575919
Chocolatey <chocolatey@realdimensions.net>
2024-01-08 00:01:19 +0000

    AU: 1 updated - pingo [skip ci] https://gist.github.com/ye4241/efab467283c72d4d480ebab4829cc1f8/b4380e4c76d047201cf7114c6103fc684b5c1c7b

--- pingo/tools/chocolateyinstall.ps1
+++ pingo/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = 'https://css-ig.net/bin/pingo-win64.zip'
-  checksum64     = '41699e11bd78d1b0491b427a1881fc3bb6849c43ab94a69f43e963dfa7e79f39'
+  checksum64     = '1b045f586fa9fe03722eef9e7f0df7d42dd25019133b14cbe16537a782ed3bf0'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
