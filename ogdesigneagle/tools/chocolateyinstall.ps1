a9ae15ff85b576b9ae501e04c950c13ef9aa162e
Chocolatey <chocolatey@realdimensions.net>
2024-07-31 12:01:55 +0000

    AU: 1 updated - ogdesigneagle [skip ci] https://gist.github.com/ye4241/efab467283c72d4d480ebab4829cc1f8/e66fa675b2b79af909ad05d094610e993f458129

--- ogdesigneagle/tools/chocolateyinstall.ps1
+++ ogdesigneagle/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://eaglefile.oss-cn-shenzhen.aliyuncs.com/releases/Eagle-3.0-build1.exe'
+  url            = 'https://eaglefile.oss-cn-shenzhen.aliyuncs.com/releases/Eagle-4.0-x64-build1.exe'
   softwareName   = 'Eagle' 
-  checksum       = 'd1cf045b812eaa8bd1131f5c841cb557307a26ffd728a768ccc5ce44fdba8598'
+  checksum       = '41bc648c2d25f825c527255784750b6f281bf75e3e664522850bfb55e66352ee'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0) 
