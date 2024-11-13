ec1a80c26dcfd23c74de63681c561707e25ebf61
Chocolatey <chocolatey@realdimensions.net>
2024-11-13 06:01:59 +0000

    AU: 1 updated - dotnetreactor [skip ci] https://gist.github.com/ye4241/efab467283c72d4d480ebab4829cc1f8/983b5f0780666d68d65c6ca2319f13990d47be00

--- dotnetreactor/tools/chocolateyinstall.ps1
+++ dotnetreactor/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.eziriz.com/download.php?download=dotnet_reactor_setup_6_9_8_0.exe'
+  url            = 'https://www.eziriz.com/download.php?download=dotnet_reactor_setup_7_0_0_0.exe'
   softwareName   = '.NET Reactor'
-  checksum       = 'b6d6ab1c8e56d7c1ef13d380751af4b07c44a8548002b317cccf242060e0b83b'
+  checksum       = 'e8ef7e09d59f5924c0bffc36b51ba5449f7c97c55782872363475e2900848bbe'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES"
   validExitCodes = @(0)
