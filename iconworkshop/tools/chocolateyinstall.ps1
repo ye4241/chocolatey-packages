792e3099ec867c9017a33d21fcfc95ae4bdbf305
ye4241 <6803102+ye4241@users.noreply.github.com>
2026-02-01 14:30:36 +0800

    Update Axialis IconWorkshop to version 6.97, adding support for 64-bit installer URLs and checksums in the update script. Adjusted chocolateyinstall.ps1 to include 32-bit and 64-bit download links and their respective checksums.

--- iconworkshop/tools/chocolateyinstall.ps1
+++ iconworkshop/tools/chocolateyinstall.ps1
@@ -1,12 +1,14 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://www.axialis.com/downloads/IconWorkshop-Pro.exe'
-  softwareName   = 'Axialis IconWorkshop*' 
-  checksum       = '814ea8d54df9d91e45380111dd477641ce40fba72f57ae6c076ccd5d05b92a83'
+  url            = 'https://www.axialis.com/downloads/IconWorkshop-Pro-32-bit.exe'
+  url64bit       = 'https://www.axialis.com/downloads/IconWorkshop-Pro.exe'
+  softwareName   = 'Axialis IconWorkshop*'
+  checksum       = 'dfe2c9c36fc1b8568855a1d1a0398a08b5c2ca1824433ecbb1fae6be048fd850'
+  checksum64     = '7ec5f959dd4d3a765ecc850f64cccf7fa0ed8b7d87485737a7f35fd1df4d8690'
   checksumType   = 'sha256'
   silentArgs     = "/q"
-  validExitCodes = @(0) 
+  validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
--- iconworkshop/update.ps1
+++ iconworkshop/update.ps1
@@ -1,4 +1,4 @@
-﻿import-module au
+import-module au
 
 $domain = 'https://www.axialis.com'
 $releases = "$domain/download/iw.html"
@@ -6,9 +6,11 @@ $releases = "$domain/download/iw.html"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.Url32)'"
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.Url64)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"      = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType32)'"
     }
   }
 }
@@ -16,13 +18,20 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $response = Invoke-WebRequest -UseBasicParsing -Uri $releases
   $content = $response.Content
-  $match = ($content | Select-String -Pattern '<p>File: <b><a href="(.*?\.exe)">IconWorkshop-Pro.exe</a></b> - Version: <b>(.*?)</b><br>').Matches[0]
-  $url = $match.Groups[1].Value
-  $version = $match.Groups[2].Value
+
+  $versionMatch = ($content | Select-String -Pattern 'var i3v\s*=\s*"([^"]+)"').Matches[0]
+  $version = $versionMatch.Groups[1].Value
+
+  $url64Match = ($content | Select-String -Pattern 'File:.*?href="(https://[^"]+/IconWorkshop-Pro\.exe)"').Matches[0]
+  $url64 = $url64Match.Groups[1].Value
+  $url32Match = ($content | Select-String -Pattern 'File:.*?href="(https://[^"]+/IconWorkshop-Pro-32-bit\.exe)"').Matches[0]
+  $url32 = $url32Match.Groups[1].Value
+
   @{
-    Url32   = $url
+    Url32   = $url32
+    Url64   = $url64
     Version = $version
   }
 }
 
-Update-Package -ChecksumFor 32
+Update-Package -ChecksumFor 32,64
